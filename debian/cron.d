2c6cea6f003c01e3c0595294d50153472b613485
Petr Jurasek <petr.jurasek@solnet.cz>
2015-01-20 16:42:05 +0100

    debian/changelog

--- debian/cron.d
+++ debian/cron.d
@@ -1,4 +1,4 @@
 # /etc/cron.d/lnbackup-ng: crontab fragment for lnbackup-ng
 
 # run default backup (localhost) every day
-00 23   * * *   root if [ -x /usr/sbin/lnbackup ]; then /usr/sbin/lnbackup --backup >> /var/log/lnbackup_out 2>&1; fi
+00 23   * * *   root if [ -x /usr/bin/lnbackup ]; then /usr/bin/lnbackup --backup >> /var/log/lnbackup_out 2>&1; fi
